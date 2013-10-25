0d923aabfdc32ed8b08517cb595028af57c2f832
jtouati-keyyo <jtouati@keyyo.com>
2013-10-25 15:29:25 +0200

    Manager client 1.0 (initial import)

---
+++ lib/Keyyo/Manager/Client.php
@@ -0,0 +1,209 @@
+<?php
+/**
+ * @license https://github.com/Keyyo/keyyo-manager-php-client/blob/master/LICENSE MIT License
+ * Copyright (c) 2013 Keyyo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace Keyyo\Manager;
+
+/**
+ * Represents a client of the Manager webservice provided by Keyyo
+ *
+ * @author Jérémy Touati <jtouati@keyyo.com>
+ */
+class Client extends Resource
+{
+	/**
+	 * The OAuth2 access token used to authenticate against the webservice
+	 * @var string
+	 */
+	protected $access_token;
+
+	/**
+	 * An array of options, e.g. the format to use for numbers, or any other Manager option
+	 * @var array
+	 */
+	protected $options = array();
+
+	/**
+	 * Constructs an instance of a client that will connect to a specific version of the Manager
+	 * @param string $version The version of the remote Manager to access (e.g. "1.0")
+	 * @param string $access_token A valid OAuth2 access token
+	 * @param array $options An array of options, e.g. the format to use for numbers, or any other Manager option
+	 */
+	public function __construct($version, $access_token, array $options = array())
+	{
+		parent::__construct($this, $this->get_webservice_url() . '/' . $version);
+		$this->access_token = $access_token;
+
+		$this->options['localized_numbers'] = !empty($options['localized_numbers']);
+	}
+
+	/**
+	 * Calls a webservice and return its body as an associative array (this should not be called from outside this library)
+	 * @param string $verb The HTTP verb to use (GET, POST or DELETE are supported)
+	 * @param string $url The webservice URL to target
+	 * @param array $data An optional data array (for POST queries)
+	 * @throws \Keyyo\Manager\Exception\Exception
+	 * @return array An associative array containing the result of the query
+	 */
+	public function query($verb, $url, array $data = array())
+	{
+		return $this->get_resource_contents($this->call_webservice($verb, $url, $data));
+	}
+
+	/**
+	 * Calls a webservice and return HTTP status code, headers and body
+   * @param string $verb The HTTP verb to use (GET, POST or DELETE are supported)
+	 * @param string $url The webservice URL to target
+	 * @param array $data An optional data array (for POST queries)
+	 * @throws \Keyyo\Manager\Exception\WebserviceQueryException
+	 * @return array An array containing three values: HTTP status code (as integer), headers (as array), body (as string)
+	 */
+	protected function call_webservice($verb, $url, array $data = array())
+	{
+		// Initialize cURL
+
+		if (!function_exists('curl_version'))
+			throw new \Keyyo\Manager\Exception\WebserviceQueryException('cURL is not available');
+
+		$curl_request = curl_init();
+		if ($curl_request === false)
+			throw new \Keyyo\Manager\Exception\WebserviceQueryException('Could not initialize cURL request');
+
+		// Append "options" GET parameters to the URI
+
+		$get_parameters = array();
+		if ($this->test_option('localized_numbers', true))
+			$get_parameters['localized_numbers'] = '1';
+
+		if (count($get_parameters) > 0)
+			$url .= (strpos($url, '?') === false ? '?' : '&') . http_build_query($get_parameters);
+
+		// Data
+
+		$data = array_map(function($value) {
+			if (is_bool($value))
+				$value = $value ? 1 : 0;
+
+			return $value;
+		}, $data);
+
+		// Build cURL option array
+
+		$curl_options = array(
+			CURLOPT_HEADER => true,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_URL => $url,
+			CURLOPT_CUSTOMREQUEST => $verb,
+			CURLOPT_POSTFIELDS => $data,
+			CURLOPT_HTTPHEADER => array('Authorization: Bearer ' . $this->access_token)
+		);
+		if (curl_setopt_array($curl_request, $curl_options) === false)
+			throw new \Keyyo\Manager\Exception\WebserviceQueryException('Could not set cURL request options (' . curl_error($curl_request) . ')');
+
+		// Execute cURL request and retrieve HTTP status code
+
+		$response = curl_exec($curl_request);
+		if ($response === false)
+			throw new \Keyyo\Manager\Exception\WebserviceQueryException('Could not execute cURL request (' . curl_error($curl_request) . ')');
+
+		if (($http_status = curl_getinfo($curl_request, CURLINFO_HTTP_CODE)) === false
+				|| ($header_size = curl_getinfo($curl_request, CURLINFO_HEADER_SIZE)) === false)
+			throw new \Keyyo\Manager\Exception\WebserviceQueryException('Could not retrieve http status code or header size (' . curl_error($curl_request) . ')');
+
+		curl_close($curl_request);
+
+		// Retrieve headers
+
+		$header = substr($response, 0, $header_size);
+
+		if (preg_match_all('/^([^:]+):\s+(.*)$/m', $header, $matches, PREG_SET_ORDER) === false)
+			throw new \Keyyo\Manager\Exception\WebserviceQueryException('Could not parse http response headers');
+
+		$headers = array();
+
+		if (count($matches) > 1)
+			foreach (array_slice($matches, 1) as $match)
+				$headers[$match[1]] = isset($match[2]) ? $match[2] : null;
+
+		// Retrieve body
+
+		$body = substr($response, $header_size);
+
+		return [$http_status, $headers, $body];
+	}
+
+	/**
+	 *
+	 * @param array $call_result An array containing three values: HTTP status code (as integer), headers (as array), body (as string)
+	 * @return array An array containing three values: HTTP status code (as integer), headers (as array), body (as string)
+	 * @throws \Keyyo\Manager\Exception\Exception
+	 * @return array An associative array containing the result of the query
+	 */
+	protected function get_resource_contents($call_result)
+	{
+		list($http_status, $headers, $body) = $call_result;
+		$status_reason = isset($headers['X-Status-Reason']) ? $headers['X-Status-Reason'] : null;
+
+		switch ($http_status)
+		{
+			case 200:
+				break;
+			case 403:
+				throw new \Keyyo\Manager\Exception\ForbiddenAccessException($status_reason);
+			case 404:
+				throw new \Keyyo\Manager\Exception\ResourceNotFoundException($status_reason);
+			case 500:
+			default:
+				throw new \Keyyo\Manager\Exception\InternalServerErrorException($status_reason);
+		}
+
+		if (is_string($body) && strlen($body) > 0)
+		{
+			$resource_contents = json_decode($body, true);
+			if (is_null($resource_contents))
+				throw new \Keyyo\Manager\Exception\WebserviceQueryException('Could not parse the request\'s response body');
+		}
+
+		else
+			$resource_contents = null;
+
+		return $resource_contents;
+	}
+
+	/**
+	 * @return string The absolute URL of the (root of the) webservice
+	 */
+	protected function get_webservice_url()
+	{
+		return 'https://api.keyyo.com/manager';
+	}
+
+	/**
+	 * @param string $name An option name
+	 * @param string $value An option value
+	 * @return boolean Whether the option with the given name exists and has the given value
+	 */
+	private function test_option($name, $value = true)
+	{
+		return isset($this->options[$name]) && $this->options[$name] === $value;
+	}
+}
---
+++ lib/Keyyo/Manager/Resource.php
@@ -0,0 +1,338 @@
+<?php
+/**
+ * @license https://github.com/Keyyo/keyyo-manager-php-client/blob/master/LICENSE MIT License
+ * Copyright (c) 2013 Keyyo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace Keyyo\Manager;
+
+/**
+ * Represents a resource retrieve and manipulated through the Client
+ *
+ * @author Jérémy Touati <jtouati@keyyo.com>
+ */
+class Resource implements \Iterator, \ArrayAccess, \Countable
+{
+	/**
+	 * A reference to the client
+	 * @var \Keyyo\Manager\Client
+	 */
+	protected $client;
+
+	/**
+	 * The absolute URL of this resource
+	 * @var string
+	 */
+	protected $url;
+
+	/**
+	 * Whether or not this resource represents a collection or an individual resource
+	 * @var boolean
+	 */
+	protected $is_collection;
+
+	/**
+	 * An associative array containing all of the resource contents
+	 * @var array
+	 */
+	protected $contents;
+
+	/**
+	 * An associative array containing all the resource properties (contents except metadata)
+	 * @var array
+	 */
+	protected $properties;
+
+	/**
+	 * Constructs a resource
+	 * @param \Keyyo\Manager\Client $client A reference to the client
+	 * @param string $url The absolute URL of this resource
+	 * @param string $resource_contents An optional array containing the contents to load this resource from
+	 */
+	public function __construct(\Keyyo\Manager\Client $client, $url, $resource_contents = null)
+	{
+		$this->client = $client;
+		$this->url = $url;
+
+		if (is_array($resource_contents))
+			$this->initialize_from_resource_contents($resource_contents);
+	}
+
+	/**
+	 * Creates a resource by adding an URL segment to the current one
+	 * @param string $method_name The name of the URL segment
+	 * @param array $parameters If this array contains one parameter, it will be added as a segment too
+	 * @return \Keyyo\Manager\Resource The new resource
+	 */
+	public function __call($method_name, array $parameters)
+	{
+		$url = $this->url . '/' . $method_name;
+		if (isset($parameters[0]))
+			$url .= '/' . $parameters[0];
+
+		return new Resource($this->client, $url);
+	}
+
+	/**
+	 * Retrieves a property from the resource
+	 * @param string $property The name of the property
+	 * @throws \Keyyo\Manager\Exception\NoSuchPropertyException
+	 * @return string The value of this property, if it does exist
+	 */
+	public function __get($property)
+	{
+		$this->fetch();
+
+		if (!isset($this->properties))
+			throw new \Keyyo\Exception\NoSuchPropertyException('No such property: ' . $property);
+
+		return $this->properties[$property];
+	}
+
+	/**
+	 * Updates a property on the resource
+	 * @param string $property The name of the property
+	 * @param string $value The value to set
+	 * @throws \Keyyo\Manager\Exception\Exception
+	 */
+	public function __set($property, $value)
+	{
+		$this->update(array($property => $value));
+	}
+
+	/**
+	 * Creates a sub-resource on a collection resource
+	 * @param array $properties An associative array of values to set on the created subresource
+	 * @return \Keyyo\Manager\Resource
+	 * @throws \Keyyo\Manager\Exception\Exception
+	 */
+	public function create(array $properties = array())
+	{
+		$resource_contents = $this->client->query('POST', $this->url, $properties);
+		return new Resource($this->client, $resource_contents['_links']['self']['href'], $resource_contents);
+	}
+
+	/**
+	 * Updates one or several properties on the resource
+	 * @param array $properties The array of properties to set
+	 */
+	public function update(array $properties = array())
+	{
+		$resource_contents = $this->client->query('POST', $this->url, $properties);
+		$this->initialize_from_resource_contents($resource_contents);
+	}
+
+	/**
+	 * Deletes the current resource
+	 * @throws \Keyyo\Manager\Exception\Exception
+	 */
+	public function delete()
+	{
+		$resource_contents = $this->client->query('DELETE', $this->url);
+		$this->initialize_from_resource_contents($resource_contents);
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see Iterator::rewind()
+	 */
+	public function rewind()
+	{
+		$this->fetch();
+		return reset($this->contents);
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see Iterator::current()
+	 */
+	public function current()
+	{
+		$this->fetch();
+		$value = current($this->contents);
+
+		if (!($value instanceof Resource))
+			$value = $this->contents[$this->key()] = new Resource($this->client, $this->client->to_url($value['_links']['self']['href']), $value);
+
+		return $value;
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see Iterator::key()
+	 */
+	public function key()
+	{
+		$this->fetch();
+		return key($this->contents);
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see Iterator::next()
+	 */
+	public function next()
+	{
+		$this->fetch();
+		return next($this->contents);
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see Iterator::valid()
+	 */
+	public function valid()
+	{
+		$this->fetch();
+		return isset($this->contents[$this->key()]);
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see ArrayAccess::offsetExists()
+	 */
+	public function offsetExists($offset)
+	{
+		$this->fetch();
+		return isset($this->contents[$offset]);
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see ArrayAccess::offsetGet()
+	 */
+	public function offsetGet($offset)
+	{
+		$this->fetch();
+		return $this->contents[$offset];
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see ArrayAccess::offsetSet()
+	 */
+	public function offsetSet($offset, $value)
+	{
+		$this->fetch();
+		$this->contents[$offset] = $value;
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see ArrayAccess::offsetUnset()
+	 */
+	public function offsetUnset($offset)
+	{
+		$this->fetch();
+		unset($this->contents[$offset]);
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see Countable::count()
+	 */
+	public function count()
+	{
+		$this->fetch();
+		return count($this->contents);
+	}
+
+	/**
+	 * @return array The contents of this resource
+	 */
+	public function get_contents()
+	{
+		$this->fetch();
+		return $this->contents;
+	}
+
+	/**
+	 * @return array The properties (contents except meta data) of this resource
+	 */
+	public function get_properties()
+	{
+		$this->fetch();
+		return $this->properties;
+	}
+
+	/**
+	 * @return string The type of this resource, or null if there is none
+	 */
+	public function get_type()
+	{
+		return isset($this->contents['_resource_type']) ? $this->contents['_resource_type'] : null;
+	}
+
+	/**
+	 * Fetches the contents of this resource from the webservice, and caches it into $contents
+	 */
+	protected function fetch()
+	{
+		if (isset($this->contents))
+			return;
+
+		$resource_contents = $this->client->query('GET', $this->url);
+
+		$this->initialize_from_resource_contents($resource_contents);
+	}
+
+	/**
+	 * Initializes the current resource from resource contents returned by the webservice
+	 * @param array $resource_contents
+	 */
+	private function initialize_from_resource_contents($resource_contents)
+	{
+		if (isset($resource_contents['_embedded']))
+		{
+			$this->is_collection = true;
+			foreach ($resource_contents['_embedded'] as $sub_resources_contents)
+			{
+				foreach ($sub_resources_contents as $sub_resource_contents)
+					$this->contents[] = $sub_resource_contents;
+			}
+			$this->properties = null;
+		}
+		else
+		{
+			$this->is_collection = false;
+			$this->contents = $resource_contents;
+			$this->properties = $this->filter_properties($this->contents);
+		}
+	}
+
+	/**
+	 * Filters contents to retrieve only its properties (non meta-data)
+	 * @param array $contents
+	 * @return array
+	 */
+	private function filter_properties($contents)
+	{
+		$properties = array();
+
+		if (is_array($contents))
+		{
+			foreach ($contents as $key => $value)
+				if (strpos($key, '_') !== 0)
+					$properties[$key] = $value;
+		}
+
+		return $properties;
+	}
+}
